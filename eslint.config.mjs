bf40f0472286bcb95be19de646a6ccf16ffac600
AroNicolas <hei.nicolas.5@gmail.com>
2025-11-20 09:37:18 +0300

    fix: run CI again very last

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,8 +4,46 @@ import json from "@eslint/json";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], rules: {"no-unused-vars": "error","no-undef": "error"}, languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
-  { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },
+  {
+    ignores: [
+      "package-lock.json",
+      "node_modules/",
+      "dist/",
+      "build/",
+    ],
+  },
+
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error"
+    },
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node }
+    }
+  },
+
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"]
+  },
+
+  {
+    files: ["**/*.jsonc"],
+    plugins: { json },
+    language: "json/jsonc",
+    extends: ["json/recommended"]
+  },
+
+  {
+    files: ["**/*.json5"],
+    plugins: { json },
+    language: "json/json5",
+    extends: ["json/recommended"]
+  }
 ]);
